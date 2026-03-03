85952a0b380266ef6c2c9a9ccd39f10249d69898
wldus2056-cell <wldus2056@gmail.com>
2026-03-03 16:00:05 +0900

    Update g2b.py

--- g2b.py
+++ g2b.py
@@ -2,32 +2,23 @@
 import requests
 from urllib.parse import quote
 
-def fetch_all_pages(url: str, params: dict, page_size: int = 999) -> list[dict]:
-    all_data = []
-    page_no = 1
-
+def fetch_all_pages(url: str, params: dict) -> list[dict]:
+    all_items = []
+    page = 1
     while True:
-        params["pageNo"] = page_no
-        params["numOfRows"] = page_size
-
+        params["pageNo"] = page
+        params["numOfRows"] = 999
         r = requests.get(url, params=params, timeout=30)
         r.raise_for_status()
-
         data = r.json()
-        body = data.get("response", {}).get("body", {})
-        items = body.get("items") or []
-
+        items = data.get("response", {}).get("body", {}).get("items") or []
         if not items:
             break
-
-        all_data.extend(items)
-
-        if len(items) < page_size:
+        all_items.extend(items)
+        if len(items) < 999:
             break
-        page_no += 1
-
-    return all_data
-
+        page += 1
+    return all_items
 
 def fetch_bid_list(api_key: str, start_dt: str, end_dt: str) -> list[dict]:
     url = "http://apis.data.go.kr/1230000/ad/BidPublicInfoService/getBidPblancListInfoServc"
@@ -39,7 +30,6 @@ def fetch_bid_list(api_key: str, start_dt: str, end_dt: str) -> list[dict]:
         "type": "json",
     }
     return fetch_all_pages(url, params)
-
 
 def fetch_prebid_list(api_key: str, start_dt: str, end_dt: str) -> list[dict]:
     url = "http://apis.data.go.kr/1230000/ao/HrcspSsstndrdInfoService/getPublicPrcureThngInfoServc"
